1ec07a3667685cdd230ea440ec92c660cea2e2a1
Laurent David <lmedavid@gmail.com>
2017-03-06 22:59:58 +0100

    Make sure we can accept a wider range of configuration such as ones with no BASIC_AUTH or different URL for XX_STAGE_BASE_URL

--- regression/pages/lms/__init__.py
+++ regression/pages/lms/__init__.py
@@ -13,8 +13,13 @@ LMS_STAGE_BASE_URL = 'courses.stage.edx.org'
 
 LMS_BASE_URL = os.environ.get('LMS_BASE_URL', LMS_STAGE_BASE_URL)
 
-LOGIN_BASE_URL = 'https://{}:{}@{}'.format(
-    BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWORD, LMS_BASE_URL
-)
+if BASIC_AUTH_PASSWORD == "not_set" or BASIC_AUTH_USERNAME == "not_set":
+    LOGIN_BASE_URL = os.environ.get('LMS_LOGIN_BASE_URL','https://{}'.format(
+         LMS_BASE_URL
+        ))
+else:
+    LOGIN_BASE_URL = os.environ.get('LMS_LOGIN_BASE_URL','https://{}:{}@{}'.format(
+            BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWORD, LMS_BASE_URL
+        ))
 
-LMS_REDIRECT_URL = 'https://stage.edx.org'
+LMS_REDIRECT_URL = os.environ.get('LMS_REDIRECT_URL','https://stage.edx.org')
